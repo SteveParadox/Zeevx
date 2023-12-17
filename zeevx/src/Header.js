8eefd13126d899b7adc8d0a3838f749675fde2a6
SteveParadox <fordstphn@gmail.com>
2023-12-17 03:35:16 +0100

    correction

--- zeevx/src/Header.js
+++ zeevx/src/Header.js
@@ -3,17 +3,24 @@ import "./Header.css";
 import Person4Icon from '@mui/icons-material/Person4';
 import IconButton from "@mui/material/IconButton";
 import AcUnitIcon from '@mui/icons-material/AcUnit';
+import ForumIcon  from '@mui/icons-material/Send';
 
 function YourComponent() {
   return (
     <div className="header">
         <IconButton>
                         <Person4Icon fontSize="large" className="header__icon" />
-                        <AcUnitIcon fontSize="large" className="header__icon" />
 
         </IconButton>
-<img className="header__logo" src=""
-/>
+    <img className="header__logo" src=""/> 
+        <IconButton>
+                    <AcUnitIcon fontSize="large" className="header__icon" />
+
+            </IconButton>    
+    <IconButton>
+            <ForumIcon  fontSize="large" className="header__icon" />
+
+    </IconButton>
     </div>
   );
 }
