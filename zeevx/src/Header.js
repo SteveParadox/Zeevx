476c49b84b987d7b3821a8e308114494136e7d13
SteveParadox <fordstphn@gmail.com>
2023-12-17 02:56:10 +0100

    Button design

--- zeevx/src/Header.js
+++ zeevx/src/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import "./Header.css";
+import Person4Icon from '@mui/icons-material/Person4';
+import IconButton from "@material-ui/core/IconButton";
+
 
 function YourComponent() {
   return (
     <div className="header">
         <h2>
-            I am Header
+        <IconButton>
+                        <Person4Icon fontSize="large" className="header__icon" />
+        </IconButton>
         </h2>
 
     </div>
