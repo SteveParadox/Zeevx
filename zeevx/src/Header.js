4311cb46f82d1a80aa8c0d8a1284beeb009f8c2d
SteveParadox <fordstphn@gmail.com>
2023-12-17 03:23:33 +0100

    update

--- zeevx/src/Header.js
+++ zeevx/src/Header.js
@@ -2,17 +2,18 @@ import React from 'react';
 import "./Header.css";
 import Person4Icon from '@mui/icons-material/Person4';
 import IconButton from "@mui/material/IconButton";
-
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 
 function YourComponent() {
   return (
     <div className="header">
-        <h2>
         <IconButton>
                         <Person4Icon fontSize="large" className="header__icon" />
-        </IconButton>
-        </h2>
+                        <AcUnitIcon fontSize="large" className="header__icon" />
 
+        </IconButton>
+<img className="header__logo" src=""
+/>
     </div>
   );
 }
