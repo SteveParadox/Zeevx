fb8aae77b261238da067b229f3e01b581bf52e30
SteveParadox <fordstphn@gmail.com>
2023-12-17 02:44:45 +0100

    ccd

--- zeevx/src/App.js
+++ zeevx/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import "./Header.css";
 
 function App() {
   return (
     <div className="App">
+    <Header/>
+      {/*cards*/}
+      {/*SwipeButtons*/}
      
     </div>
   );
---
+++ zeevx/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Header.css";
+
+function YourComponent() {
+  return (
+    <div className="header">
+        <h2>
+            I am Header
+        </h2>
+
+    </div>
+  );
+}
+
+export default YourComponent;
