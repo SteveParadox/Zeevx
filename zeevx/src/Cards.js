1ddd192126d777b7daaccca226d5577543d99a76
SteveParadox <fordstphn@gmail.com>
2023-12-19 04:05:19 +0100

    relay

--- zeevx/src/Cards.js
+++ zeevx/src/Cards.js
@@ -6,17 +6,17 @@ function Cards() {
   const [people, setPeople] = useState([
     {
       name: 'Cristiano Ronaldo',
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Cristiano_Ronaldo_WC2022_-_01_%28cropped%29.jpg/188px-Cristiano_Ronaldo_WC2022_-_01_%28cropped%29.jpg',
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Cristiano_Ronaldo_WC2022_-_01_%28cropped%29.jpg/188px-Cristiano_Ronaldo_WC2022_-_01_%28cropped%29.jpg'
 
     },
     {
       name: 'Kelvin De Bruyne',
-      url: 'https://footballwhispers.com/wp-content/uploads/2021/03/Bruyne.jpg',
+      url: 'https://footballwhispers.com/wp-content/uploads/2021/03/Bruyne.jpg'
 
     },
     {
       name: 'Lioniel Messi',
-      url: 'https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2012/12/9/1355055907038/e0428d50-868a-4840-af87-b94579a94da4-460.jpeg',
+      url: 'https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2012/12/9/1355055907038/e0428d50-868a-4840-af87-b94579a94da4-460.jpeg'
 
     }
   ]);
