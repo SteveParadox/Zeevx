a8981a30e42fd408948e75da7d0cc158098dab92
SteveParadox <fordstphn@gmail.com>
2023-12-19 04:09:13 +0100

    edit

--- zeevx/src/Cards.js
+++ zeevx/src/Cards.js
@@ -35,19 +35,21 @@ function Cards() {
     <div className="Cards">
       <div className="Card__cardContainer">
        {people.map((person) =>  (
-      <TinderCard className="swipe"
-                  key={person.name}
-                  preventSwipe={["up","down"]}
-                  onSwipe={(dir) => swiped[dir, person.name]}
-                  onCardLeftScreen = {() => outOfFrame(person.name)}
-                  >
-              <div
-              style={{ backgroundImage: `url(${person.url})` }}
-              className="card"
-            >
-              <h3>{person.name}</h3>
-            </div>
-      </TinderCard>
+      <TinderCard
+      className="swipe"
+      key={person.name}
+      preventSwipe={["up", "down"]}
+      onSwipe={(dir) => swiped(dir, person.name)}
+      onCardLeftScreen={() => outOfFrame(person.name)}
+    >
+      <div
+        style={{ backgroundImage: `url(${person.url})` }}
+        className="card"
+      >
+        <h3>{person.name}</h3>
+      </div>
+    </TinderCard>
+    
       )) }
       </div>
     </div>
