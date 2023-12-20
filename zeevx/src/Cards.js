374e86e48e8f45cd32f41bcd0ab14cde2ef95b69
SteveParadox <fordstphn@gmail.com>
2023-12-21 00:28:27 +0100

    depreciated

--- zeevx/src/Cards.js
+++ zeevx/src/Cards.js
@@ -3,22 +3,58 @@ import TinderCard from "react-tinder-card";
 import "./Cards.css";
 
 function Cards() {
+  const [people, setPeople] = useState([
+    {
+      name: 'Cristiano Ronaldo',
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Cristiano_Ronaldo_WC2022_-_01_%28cropped%29.jpg/188px-Cristiano_Ronaldo_WC2022_-_01_%28cropped%29.jpg'
 
+    },
+    {
+      name: 'Kelvin De Bruyne',
+      url: 'https://footballwhispers.com/wp-content/uploads/2021/03/Bruyne.jpg'
 
-  const onSwipe = (direction) => {
-    console.log('You swiped: ' + direction)
-  }
-  
-  const onCardLeftScreen = (myIdentifier) => {
-    console.log(myIdentifier + ' left the screen')
-  }
+    },
+    {
+      name: 'Lioniel Messi',
+      url: 'https://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2012/12/9/1355055907038/e0428d50-868a-4840-af87-b94579a94da4-460.jpeg'
 
+    }
+  ]);
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing " + nameToDelete);
+    //setLastDirection(direction);
+
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen")
+  };
 
   return (
+    <div className="Cards">
+      <div className="Card__cardContainer">
+       {people.map((person) =>  (
+      <TinderCard
+      className="swipe"
+      key={person.name}
+      preventSwipe={["up", "down"]}
+      onSwipe={(dir) => swiped(dir, person.name)}
 
-      <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']}>Hello, World!</TinderCard>
-
-   
+      onCardLeftScreen={() => outOfFrame(person.name)}
+    >
+  
+      <div
+        style={{ backgroundImage: `url(${person.url})` }}
+        className="card"
+      >
+        <h3>{person.name}</h3>
+      </div>
+    </TinderCard>
+    
+      )) }
+      </div>
+    </div>
   );
 }
 
