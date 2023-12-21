0340a4ffaac672408b2eca52edffeda77dbed372
SteveParadox <fordstphn@gmail.com>
2023-12-22 00:40:32 +0100

    backend update

--- Backend/ReadME.md
+++ Backend/ReadME.md
@@ -1,1 +1,3 @@
 Hello
+
+JOvRV8fE35skPjEp
---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+
+// App Config
+const app = express();
+const port = process.env.PORT || 8001
+
+// Middlewares
+
+//Db Config
+
+//Api Endpoint
+app.get('/', (req, res) => res.status(200).
+send(
+"   Hello World"
+));
+// Listener
+
+app.listen(port,  () => console.log(`listening on localhost: ${port}`));
+
