5b10333e7f96913ad375cc697b87d4f23e76470c
SteveParadox <fordstphn@gmail.com>
2023-12-22 02:01:26 +0100

    push end

--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,57 @@
 import express from "express";
 import mongoose from "mongoose";
+import Cors from "cors"
+import Cards from './dbCards.js'
 
 // App Config
 const app = express();
 const port = process.env.PORT || 8001
+const uri = 'mongodb+srv://fordstphn:JOvRV8fE35skPjEp@cluster0.ronso6r.mongodb.net/?retryWrites=true&w=majority';
 
 // Middlewares
+app.use(express.json());
+app.use(Cors());
+
 
 //Db Config
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
 
 //Api Endpoint
 app.get('/', (req, res) => res.status(200).
 send(
 "   Hello World"
 ));
-// Listener
 
+app.post('/cards', (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) =>{
+        if (err) {
+            res.status(500).send(err)
+
+        } else{
+            res.status(201).send(data)
+        }
+    })
+
+});
+
+app.get('/cards', (req, res) => {
+
+    Cards.find((err, data) =>{
+        if (err) {
+            res.status(500).send(err)
+
+        } else{
+            res.status(200).send(data)
+        }
+    })
+
+});
+// Listener
 app.listen(port,  () => console.log(`listening on localhost: ${port}`));
 
