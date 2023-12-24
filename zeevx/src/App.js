d358eb055049ab9bc42a24c1c755934b43057e44
SteveParadox <fordstphn@gmail.com>
2023-12-24 03:08:14 +0100

    adding Login component

--- zeevx/src/App.js
+++ zeevx/src/App.js
@@ -1,17 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from "./Header.js";
 import Cards from "./Cards.js";
 import SwipeButtons from "./swipeButtons.js";
+import Login from './Login';
 
 function App() {
   return (
-    <div className="App">
-    <Header/>
-	<Cards /> 
-	 <SwipeButtons/> 
-	 
-     
-    </div>
+    <Router>
+      <div className="App">
+       
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Cards />
+            <SwipeButtons />
+            {/* Add more components or content as needed */}
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ zeevx/src/Login.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implement your login logic here
+    // You can use state values (username and password) for authentication
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <br />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
