63bd3809d1d14c1247cc145e4313ba8bf576ae4e
SteveParadox <fordstphn@gmail.com>
2023-12-24 03:31:26 +0100

    adding google auth

--- zeevx/src/Auth/Login.js
+++ zeevx/src/Auth/Login.js
@@ -1,17 +1,31 @@
 import React, { useState } from 'react';
+import { auth, provider } from './firebase';
+import { signInWithPopup } from 'firebase/auth';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = () => {
-    // Implement your login logic here
+    // Implement your traditional login logic here
     // You can use state values (username and password) for authentication
+  };
+
+  const handleGoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      console.log('Logged in with Google:', user);
+      // Add your logic after successful Google login
+    } catch (error) {
+      console.error('Error signing in with Google:', error.message);
+    }
   };
 
   return (
     <div>
       <h2>Login</h2>
+      {/* Your existing login form */}
       <label>
         Username:
         <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
@@ -23,6 +37,9 @@ const Login = () => {
       </label>
       <br />
       <button onClick={handleLogin}>Login</button>
+
+      {/* Google login button */}
+      <button onClick={handleGoogleLogin}>Login with Google</button>
     </div>
   );
 };
