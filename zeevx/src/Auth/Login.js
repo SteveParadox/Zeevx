8e921c2ca1af94f52c8c43abd3ef1d5e66dc2df6
SteveParadox <fordstphn@gmail.com>
2023-12-24 03:34:01 +0100

    update

--- zeevx/src/Auth/Login.js
+++ zeevx/src/Auth/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { auth, provider } from './firebase.js';
+import { auth, provider } from './Firebase.js';
 import { signInWithPopup } from 'firebase/auth';
 
 const Login = () => {
