35a56f128cd2ffc834db1943dfe8519cb466cb58
SteveParadox <fordstphn@gmail.com>
2023-12-24 16:27:59 -0800

    Pull update

--- zeevx/src/Auth/Auth.js
+++ zeevx/src/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
-import { auth } from './firebase'; 
+import { auth } from './Firebase.js'; 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
--- zeevx/src/index.js
+++ zeevx/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AuthProvider } from './Auth'; // Assuming you have an Auth.js file
+import { AuthProvider } from './Auth/Auth.js'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
