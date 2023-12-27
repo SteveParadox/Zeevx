8a010611288db239885dc0f7e8cf0da0fe553ca8
SteveParadox <fordstphn@gmail.com>
2023-12-26 16:12:06 -0800

    Update

--- zeevx/src/Home/LandingPage.js
+++ zeevx/src/Home/LandingPage.js
@@ -11,7 +11,7 @@ const LandingPage = () => {
     <div class="nav-bar">
       <div class="container">
         <a class="logo-nav" href="">
-          Remember<span>That</span>
+          Zee<span>VX</span>
         </a>
         <img
           id="mobile-cta"
@@ -71,22 +71,6 @@ const LandingPage = () => {
     </section>
     {/* <!--SECTION HERO BLOG END--> */}
 
-    {/* <!--SECTION FEATURES BLOG START--> */}
-    <section class="features-section">
-      <div class="container">
-        <ul>
-          <li>unlimited tasks</li>
-          <li>confetti Explosions upon task completions</li>
-          <li>real time collaboration</li>
-          <li>sms task reminder</li>
-          <li>social media announcement</li>
-          <li>other awesome features</li>
-        </ul>
-      </div>
-      <img src='' alt="phone" />
-    </section>
-    {/* <!--SECTION FEATURES BLOG END--> */}
-
     {/* <!--SECTION TEST  BLOG START--> */}
     <section class="test-monials-section">
       <div class="container">
@@ -123,46 +107,7 @@ const LandingPage = () => {
         </ul>
       </div>
     </section>
-    {/* <!--SECTION TEST  BLOG END--> */}
-
-    {/* <!--SECTION CONTACT  BLOG START--> */}
-    <section class="contact-section">
-      <div class="container">
-        <div class="contact-left">
-          <h2>Contact</h2>
-          <form action="#" method="post">
-            <label for="name">Name</label>
-            <input type="text" id="name" name="name" />
-            <label for="message">Message</label>
-            <textarea
-              name="message"
-              id="message"
-              cols="30"
-              rows="10"
-            ></textarea>
-
-            <input type="submit" value="send message" class="send-message" />
-          </form>
-        </div>
-
-        <div class="contact-left">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31465.03369795242!2d80.01091702588964!3d9.669999413076031!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afe53fd7be66aa5%3A0xc7da0d9203baf512!2sJaffna!5e0!3m2!1sen!2slk!4v1631203020561!5m2!1sen!2slk"
-            width="600"
-            height="450"
-            // style="border:0"
-            allowfullscreen=""
-            loading="lazy"
-            title="Lazy"
-          ></iframe>
-        </div>
-      </div>
-    </section>
-
-    {/* <!--JAVASCRIPT START-->
-   <script>
-          
-   </script> */}
+ 
   </div>
   );
 };
