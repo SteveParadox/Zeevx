8fd710f850a44b2777441bdf0efeffa8e719e1b0
SteveParadox <fordstphn@gmail.com>
2023-12-29 16:14:59 -0800

    Push Firebase Update

---
+++ Backend/DB/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Hash the password before saving it to the database
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+
+  try {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// Method to compare passwords during login
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- zeevx/src/Auth/Firebase.js
+++ zeevx/src/Auth/Firebase.js
@@ -1,18 +1,19 @@
-// firebase.js
-
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: "AIzaSyBXYWZzVmpSFKDyNvfcZ-V91I_poXlHJA4",
+  authDomain: "zeevx-90c48.firebaseapp.com",
+  projectId: "zeevx-90c48",
+  storageBucket: "zeevx-90c48.appspot.com",
+  messagingSenderId: "714618924637",
+  appId: "1:714618924637:web:a9a4deff16f123ccf2a7b4",
+  measurementId: "G-HNZRHSLPTX"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(firebaseApp);
 const provider = new GoogleAuthProvider();
 
