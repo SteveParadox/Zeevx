79bacade3ced142e2b700606ae06587ab6eaa998
SteveParadox <fordstphn@gmail.com>
2024-01-02 00:30:13 +0100

    ahhhhhhhhhhhhhhhhhhhhhhhhhhh

--- zeevx/src/Home/Cards.js
+++ zeevx/src/Home/Cards.js
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Redirect } from 'react-router-dom';
-import TinderCard from "react-tinder-card";
-import "../Css/Cards.css";
+import { useNavigate } from 'react-router-dom';
+import TinderCard from 'react-tinder-card';
+import '../Css/Cards.css';
 import axios from '../Utils/axios';
 import { useAuth } from '../Auth/Auth.js';
 
 function Cards() {
   const [people, setPeople] = useState([]);
-  const [redirectLogin, setRedirectLogin] = useState(false);
+  const navigate = useNavigate();
   const user = useAuth();
 
   useEffect(() => {
     if (!user) {
-      // Set the state to trigger redirection
-      setRedirectLogin(true);
+      navigate('/login');
     } else {
       async function fetchData() {
         const req = await axios.get('/cards');
@@ -22,20 +21,15 @@ function Cards() {
 
       fetchData();
     }
-  }, [user]);
+  }, [user, navigate]);
 
   const swiped = (direction, nameToDelete) => {
-    console.log("removing " + nameToDelete);
+    console.log('removing ' + nameToDelete);
   };
 
   const outOfFrame = (name) => {
-    console.log(name + " left the screen");
+    console.log(name + ' left the screen');
   };
-
-  // Render the Redirect component if redirectLogin is true
-  if (redirectLogin) {
-    return <Redirect to='/login' />;
-  }
 
   return (
     <div className="Cards">
@@ -44,14 +38,11 @@ function Cards() {
           <TinderCard
             className="swipe"
             key={person.name}
-            preventSwipe={["up", "down"]}
+            preventSwipe={['up', 'down']}
             onSwipe={(dir) => swiped(dir, person.name)}
             onCardLeftScreen={() => outOfFrame(person.name)}
           >
-            <div
-              style={{ backgroundImage: `url(${person.imgUrl})` }}
-              className="card"
-            >
+            <div style={{ backgroundImage: `url(${person.imgUrl})` }} className="card">
               <h3>{person.name}</h3>
             </div>
           </TinderCard>
