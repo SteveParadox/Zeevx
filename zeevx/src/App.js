8b5d45841ea81905e46eed71fccd8d364e1f70ab
SteveParadox <fordstphn@gmail.com>
2024-01-03 01:10:01 +0100

    jndonognvfd

--- zeevx/src/App.js
+++ zeevx/src/App.js
@@ -23,26 +23,28 @@ const App = () => {
 
   return (
     <Router>
-     
-        <Fragment>
+      <Fragment>
         <div className="App">
-        {!isLoginPage && !isLandingPage && <Header />}
-
-        <Routes>
-          <Route path='/login' element={<Login />} />
-
-          <Route path='/' element={<PrivateRoute />}>
-            <Route index element={<LandingPage />} />
-            <Route path='/home' element={<>
-              <Cards />
-              <SwipeButtons />
-              {/* Add more components or content as needed */}
-            </>} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/test' element={<Test />} />
-            <Route path='/upload' element={<Upload />} />
-          </Route>
-        </Routes>
+          {!isLoginPage && !isLandingPage && <Header />}
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Route index element={<LandingPage />} />
+                  <Route path="/home" element={<>
+                    <Cards />
+                    <SwipeButtons />
+                    {/* Add more components or content as needed */}
+                  </>} />
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="/test" element={<Test />} />
+                  <Route path="/upload" element={<Upload />} />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
         </div>
       </Fragment>
     </Router>
