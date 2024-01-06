c85360881e6495595b34a01b233b1994b2bd0a7d
SteveParadox <fordstphn@gmail.com>
2024-01-06 13:34:14 +0100

    helper

--- Backend/Utils/helpers.js
+++ Backend/Utils/helpers.js
@@ -2,17 +2,17 @@ import bcrypt from 'bcryptjs';
 import httpStatus from 'http-status';
 import ApiError from './ApiError.js';
 
-export const helper = {};
-
-helper.acceptableCountries = ['nigeria'];
-helper.acceptableGender = ['male', 'female'];
-
-helper.comparePassword = async function (oldPassword, newPassword) {
-    try {
-        const compare = await bcrypt.compare(newPassword, oldPassword);
-        return compare;
-    } catch (error) {
-        throw new ApiError("Error with comparing password", httpStatus.BAD_REQUEST, error);
+const helper = {
+    acceptableCountries: ['nigeria'],
+    acceptableGender: ['male', 'female'],
+    
+    comparePassword: async function (oldPassword, newPassword) {
+        try {
+            const compare = await bcrypt.compare(newPassword, oldPassword);
+            return compare;
+        } catch (error) {
+            throw new ApiError("Error with comparing password", httpStatus.BAD_REQUEST, error);
+        }
     }
 };
 
