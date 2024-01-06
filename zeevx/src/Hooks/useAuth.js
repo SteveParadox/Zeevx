2620bf7f3dd15a554ec45f900f58c21a077a63a5
SteveParadox <fordstphn@gmail.com>
2024-01-06 01:29:55 +0100

    update

--- zeevx/src/Hooks/useAuth.js
+++ zeevx/src/Hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import AuthProvider from '../Auth/Auth.js';
+import { AuthProvider } from '../Auth/Auth.js';
 
 const useAuth = () => {
     return useContext(AuthProvider);
