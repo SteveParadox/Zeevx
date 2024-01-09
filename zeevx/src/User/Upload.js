bb43343fe3593bb0986b7af8fc43fc290c2a115f
SteveParadox <fordstphn@gmail.com>
2024-01-09 03:31:43 +0100

    dhhg

--- zeevx/src/User/Upload.js
+++ zeevx/src/User/Upload.js
@@ -12,7 +12,7 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-// import CssBaseline from '@mui/material/CssBaseline';
+import CssBaseline from '@mui/material/CssBaseline';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -143,6 +143,7 @@ function Upload() {
       </div>
 
       <ThemeProvider theme={defaultTheme}>
+        <CssBaseline />
 
         <main>
           {/* Hero unit */}
