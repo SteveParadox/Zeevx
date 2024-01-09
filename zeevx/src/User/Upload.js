3dc621e21d632933272cd02f47afe04a8807be67
SteveParadox <fordstphn@gmail.com>
2024-01-09 03:16:50 +0100

    push

--- zeevx/src/User/Upload.js
+++ zeevx/src/User/Upload.js
@@ -147,34 +147,44 @@ function Upload() {
 
         <main>
           {/* Hero unit */}
-          <Box
-            sx={{
-              bgcolor: 'background.paper',
-              pt: 8,
-              pb: 6,
-            }}
-          >
-            <Container maxWidth="sm">
-              <Typography
-                component="h1"
-                variant="h2"
-                align="center"
-                color="text.primary"
-                gutterBottom
-              >
-                Album layout
-              </Typography>
-              <Typography variant="h5" align="center" color="text.secondary" paragraph>
-                Something short and leading about the collection below—its contents,
-                the creator, etc. Make it short and sweet, but not too short so folks
-                don&apos;t simply skip over it entirely.
-              </Typography>
-              <Fab color="secondary" aria-label="add" onClick={handleButtonClick}>
-                <AddIcon />
-              </Fab>
-            </Container>
-          </Box>
-
+  
+          <Container sx={{ py: 8 }} maxWidth="md">
+            {/* End hero unit */}
+            
+            <Grid container spacing={4}>
+              {images.map((item) => (
+                <Grid item key={item.id} xs={12} sm={6} md={4}>
+                  <Card
+                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                  >
+                    <CardMedia
+                      component="div"
+                      sx={{
+                        // 16:9
+                        pt: '56.25%',
+                      }}
+                      image={item.imageUrl} // Replace with your actual data structure
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {item.title} {/* Replace with your actual data structure */}
+                      </Typography>
+                      <Typography>
+                        {item.description} {/* Replace with your actual data structure */}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">View</Button>
+                      <Button size="small">Edit</Button>
+                      <Button variant="outlined" color="error" size="small">
+                        Delete
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
         </main>
       {/* Footer */}
       <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
