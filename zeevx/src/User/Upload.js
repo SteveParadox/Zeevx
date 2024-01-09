43f5b8be7fdd6c53d81eafc907aa7499b310c219
SteveParadox <fordstphn@gmail.com>
2024-01-09 03:12:33 +0100

    push

--- zeevx/src/User/Upload.js
+++ zeevx/src/User/Upload.js
@@ -143,7 +143,35 @@ function Upload() {
       </div>
 
       <ThemeProvider theme={defaultTheme}>
+        <CssBaseline />
 
+        <main>
+          {/* Hero unit */}
+          <Box
+            sx={{
+              bgcolor: 'background.paper',
+              pt: 8,
+              pb: 6,
+            }}
+          >
+            
+          </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
     </ThemeProvider>
     </>
   );
