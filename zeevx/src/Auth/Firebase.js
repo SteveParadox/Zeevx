3730e405eff6e620593a0a033b32c76d6bde8067
SteveParadox <fordstphn@gmail.com>
2024-01-12 02:21:23 +0100

    del

--- zeevx/src/Auth/Firebase.js
+++ zeevx/src/Auth/Firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getAnalytics } from "firebase/analytics";
-import 'firebase/storage';
 
 
 const firebaseConfig = {
