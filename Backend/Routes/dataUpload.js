089f256dc3be9c3db658d193787e65283453a890
SteveParadox <fordstphn@gmail.com>
2024-01-17 03:24:54 +0100

    push update

--- Backend/Routes/dataUpload.js
+++ Backend/Routes/dataUpload.js
@@ -23,7 +23,8 @@ router.get('/user/:userId/images', async (req, res) => {
 router.post('/upload', async (req, res) => {
     try {
       const { name, imgUrl } = req.body;
-      // const userId = req.user._id;
+      const userId = req.user._id;
+      console.log(userId);
   
       const image = new Image({
         name: name,
