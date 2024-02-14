bce22e30e6e906a019514f8c065b3a65e21cf39e
SteveParadox <fordstphn@gmail.com>
2024-02-15 00:27:57 +0100

    push all changes

--- zeevx/src/Service/VideoCall.js
+++ zeevx/src/Service/VideoCall.js
@@ -3,12 +3,21 @@ import React, { useEffect, useRef, useState } from 'react';
 function VideoCallComponent(){
   const [incall, setInCall] = useState(false);
 
-  return(
-    <div className="">
-    { incall ? "We are in call" : "waiting to join call"}
+  return (
+    <div className="App" style={{ height: "100%" }}>
+      {inCall ? (
+        <VideoCall setInCall={setInCall} />
+      ) : (
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setInCall(true)}
+        >
+          Join Call
+        </Button>
+      )}
     </div>
-  )
-
+  );
 }
 
 
